ed040c901ae66b3c672658abbabbe67e15121107
Jérôme LELEU <leleuj@gmail.com>
2022-08-16 18:28:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+<p align="center">
+  <img src="https://pac4j.github.io/pac4j/img/logo-spring-webflux.png" width="300" />
+</p>
+
+This `spring-webflux-pac4j-boot-demo` project is a Spring Boot application secured by the [spring-webflux-pac4j](https://github.com/pac4j/spring-webflux-pac4j) security library.
+
+## Run and test
+
+You can build the project and run it on [http://localhost:8080](http://localhost:8080) using the following commands:
+
+    cd spring-webflux-pac4j-boot-demo
+    mvn clean compile exec:java
+
+or
+
+    cd spring-webflux-pac4j-boot-demo
+    mvn spring-boot:run
---
+++ src/main/java/org/pac4j/demo/spring/Application.java
@@ -0,0 +1,54 @@
+package org.pac4j.demo.spring;
+
+import org.pac4j.core.context.WebContext;
+import org.pac4j.core.context.session.SessionStore;
+import org.pac4j.core.exception.http.HttpAction;
+import org.pac4j.core.profile.ProfileManager;
+import org.pac4j.springframework.context.SpringWebfluxSessionStore;
+import org.pac4j.springframework.context.SpringWebfluxWebContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.util.Map;
+
+@Controller
+public class Application {
+
+    @RequestMapping("/")
+    public String root(final ServerWebExchange exchange, final Map<String, Object> map) throws HttpAction {
+        return index(exchange, map);
+    }
+
+    @RequestMapping("/index.html")
+    public String index(final ServerWebExchange exchange, final Map<String, Object> map) throws HttpAction {
+        final WebContext context = new SpringWebfluxWebContext(exchange);
+        final SessionStore sessionStore = new SpringWebfluxSessionStore(exchange);
+        final ProfileManager profileManager = new ProfileManager(context, sessionStore);
+        map.put("profiles", profileManager.getProfiles());
+        map.put("sessionId", sessionStore.getSessionId(context, false).orElse("nosession"));
+        return "index";
+    }
+
+    @RequestMapping(path={"/twitter/index.html", "/cas/index.html", "/dba/index.html", "/protected/index.html"})
+    public String protect(final ServerWebExchange exchange, final Map<String, Object> map) {
+        return protectedIndex(exchange, map);
+    }
+
+    @RequestMapping("/info.html")
+    public String info(final ServerWebExchange exchange, final Map<String, Object> map) {
+        final SessionStore sessionStore = new SpringWebfluxSessionStore(exchange);
+        final double averageWaitTime = SpringWebfluxSessionStore.getWaitedTime() / SpringWebfluxSessionStore.getNbWaitCalls();
+        map.put("averageWaitTime", averageWaitTime);
+        map.put("sessionStore", sessionStore);
+        return "info";
+    }
+
+    protected String protectedIndex(final ServerWebExchange exchange, final Map<String, Object> map) {
+        final WebContext context = new SpringWebfluxWebContext(exchange);
+        final SessionStore sessionStore = new SpringWebfluxSessionStore(exchange);
+        final ProfileManager profileManager = new ProfileManager(context, sessionStore);
+        map.put("profiles", profileManager.getProfiles());
+        return "protectedIndex";
+    }
+}
